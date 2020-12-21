873aac7c60679c00174a877bafb511515ae162ad
821826173@qq.com <520201314wg>
2020-12-21 17:50:43 +0800

    ~

--- typings.d copy.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css';
-declare module '*.less';
-declare var Swiper: any;
-declare var mui: any;
-declare namespace JSX {
-    interface IntrinsicElements {
-        [elemName: string]: any;
-    }
-}
